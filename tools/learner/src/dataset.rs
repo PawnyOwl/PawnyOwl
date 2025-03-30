c4f04dd97476b9168008ea84ba8e20a2f03efd6e
Anton Kernazhytski <kernozh_ant2003@mail.ru>
2025-03-31 00:05:21 +0300

    Added model learner code

---
+++ tools/learner/src/dataset.rs
@@ -0,0 +1,119 @@
+use std::str::FromStr;
+
+use itertools::multiunzip;
+
+use burn::{data::dataloader::batcher::Batcher, prelude::*};
+
+use pawnyowl_board::{Cell, Color, Sq};
+use pawnyowl_eval::{Board, layers::feature::FeatureLayer};
+
+pub enum GameResult {
+    WhiteWins,
+    Draw,
+    BlackWins,
+}
+
+impl GameResult {
+    pub fn target(self) -> f64 {
+        match self {
+            Self::WhiteWins => 1.0,
+            Self::Draw => 0.5,
+            Self::BlackWins => 0.0,
+        }
+    }
+}
+
+fn split_last_space(s: &str) -> (String, String) {
+    if let Some(last_space) = s.rfind(' ') {
+        let (before, after) = s.split_at(last_space);
+        (before.to_string(), after[1..].to_string())
+    } else {
+        (String::new(), s.to_string())
+    }
+}
+
+fn parse_result(s: &str) -> GameResult {
+    match s {
+        "\"1-0\";" => GameResult::WhiteWins,
+        "\"1/2-1/2\";" => GameResult::Draw,
+        "\"0-1\";" => GameResult::BlackWins,
+        _ => panic!("Unknown game result"),
+    }
+}
+
+#[derive(Clone)]
+pub struct BoardBatcher<B: Backend> {
+    device: B::Device,
+}
+
+impl<B: Backend> BoardBatcher<B> {
+    pub fn new(device: B::Device) -> Self {
+        Self { device }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct BoardBatch<B: Backend> {
+    pub features: Tensor<B, 2>,
+    pub stages: Tensor<B, 2, Float>,
+    pub targets: Tensor<B, 2, Float>,
+}
+
+impl<B: Backend> Batcher<String, BoardBatch<B>> for BoardBatcher<B> {
+    fn batch(&self, items: Vec<String>) -> BoardBatch<B> {
+        let parse_items = |line: &String| {
+            let (fen, result) = split_last_space(line);
+            let mut fen = fen;
+            fen.truncate(fen.len() - 2);
+            fen.push_str("0 1");
+            let board = match Board::from_str(&fen) {
+                Ok(board) => board,
+                Err(e) => {
+                    panic!("{:?}", e);
+                }
+            };
+
+            let mut values = [0_i8; 64 * 6];
+            let mut stage = 0;
+            for sq in Sq::iter() {
+                let cell = board.get(sq);
+                if cell != Cell::None {
+                    if cell.color().unwrap() == Color::White {
+                        values[cell.piece().unwrap().index() * 64 + sq.index()] += 1;
+                    } else {
+                        values[cell.piece().unwrap().index() * 64 + sq.flipped_rank().index()] -= 1;
+                    }
+                    stage += FeatureLayer::STAGE_WEIGHTS[cell.index()];
+                }
+            }
+            let result = parse_result(&result).target();
+            (
+                Tensor::<B, 2>::from_data(
+                    TensorData::from([values; 1]).convert::<B::FloatElem>(),
+                    &self.device,
+                ),
+                Tensor::<B, 2, Float>::from_data(
+                    TensorData::from([[stage; 1]; 1]).convert::<B::FloatElem>(),
+                    &self.device,
+                ),
+                Tensor::<B, 2, Float>::from_data(
+                    TensorData::from([[result; 1]; 1]).convert::<B::FloatElem>(),
+                    &self.device,
+                ),
+            )
+        };
+
+        let (features, stages, targets) =
+            multiunzip(items.iter().map(parse_items).collect::<Vec<_>>());
+
+        let features = Tensor::cat(features, 0).to_device(&self.device);
+        let stages = Tensor::cat(stages, 0).to_device(&self.device);
+        let targets = Tensor::cat(targets, 0).to_device(&self.device);
+
+        BoardBatch {
+            features,
+            stages,
+            targets,
+        }
+    }
+}
---
+++ tools/learner/src/learn.rs
@@ -0,0 +1,250 @@
+use std::io::BufReader;
+use std::{fs::File, io::BufRead};
+
+use burn::backend::Autodiff;
+use burn::backend::ndarray::NdArray;
+use burn::data::dataloader::DataLoaderBuilder;
+use burn::data::dataset::Dataset;
+use burn::nn::Sigmoid;
+use burn::nn::loss::MseLoss;
+use burn::optim::AdamConfig;
+use burn::tensor::Float;
+use burn::train::metric::LossMetric;
+use burn::train::{RegressionOutput, TrainOutput, TrainStep, ValidStep};
+use burn::{
+    config::Config,
+    module::Module,
+    nn::{Linear, LinearConfig},
+    tensor::{
+        Tensor,
+        backend::{AutodiffBackend, Backend},
+    },
+    train::LearnerBuilder,
+};
+use burn_ndarray::NdArrayDevice;
+
+use crate::dataset::{BoardBatch, BoardBatcher};
+
+struct MainDataset {
+    items: Vec<String>,
+}
+
+impl MainDataset {
+    pub fn new(items: Vec<String>) -> Self {
+        Self { items }
+    }
+}
+
+impl Dataset<String> for MainDataset {
+    fn get(&self, index: usize) -> Option<String> {
+        self.items.get(index).cloned()
+    }
+
+    fn len(&self) -> usize {
+        self.items.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    fn iter(&self) -> burn::data::dataset::DatasetIterator<'_, String>
+    where
+        Self: Sized,
+    {
+        burn::data::dataset::DatasetIterator::new(self)
+    }
+}
+
+fn read_lines(filename: &str) -> Result<Vec<String>, std::io::Error> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    reader.lines().collect()
+}
+
+fn split_lines(items: Vec<String>) -> (Vec<String>, Vec<String>) {
+    let mut items = items;
+
+    let split_at = items.len() * 99 / 100;
+    let second = items.split_off(split_at);
+
+    (items, second)
+}
+
+#[derive(Config)]
+struct TrainingConfig {
+    pub model: ModelConfig,
+    pub optimizer: AdamConfig,
+    #[config(default = 128)]
+    pub num_epochs: usize,
+    #[config(default = 32768)]
+    pub batch_size: usize,
+    #[config(default = 4)]
+    pub num_workers: usize,
+    #[config(default = 42)]
+    pub seed: u64,
+    #[config(default = 1.0e-2)]
+    pub learning_rate: f64,
+}
+
+#[derive(Module, Debug)]
+struct Model<B: Backend> {
+    linear: Linear<B>,
+    sigmoid: Sigmoid,
+}
+
+#[derive(Config, Debug)]
+struct ModelConfig {
+    _unused: bool,
+}
+
+impl ModelConfig {
+    pub fn init<B: Backend>(&self, device: &B::Device) -> Model<B> {
+        Model {
+            linear: LinearConfig::new(64 * 6, 2).with_bias(false).init(device),
+            sigmoid: Sigmoid::new(),
+        }
+    }
+}
+
+impl<B: AutodiffBackend> TrainStep<BoardBatch<B>, RegressionOutput<B>> for Model<B> {
+    fn step(&self, batch: BoardBatch<B>) -> TrainOutput<RegressionOutput<B>> {
+        let item = self.forward_regression(batch.features, batch.stages, batch.targets);
+
+        TrainOutput::new(self, item.loss.backward(), item)
+    }
+}
+
+impl<B: Backend> ValidStep<BoardBatch<B>, RegressionOutput<B>> for Model<B> {
+    fn step(&self, batch: BoardBatch<B>) -> RegressionOutput<B> {
+        self.forward_regression(batch.features, batch.stages, batch.targets)
+    }
+}
+
+impl<B: Backend> Model<B> {
+    pub fn forward(&self, features: Tensor<B, 2>, stages: Tensor<B, 2>) -> Tensor<B, 2> {
+        let res = self.linear.forward(features);
+        let n = res.dims()[0];
+        let o = res.clone().slice([0..n, 0..1]);
+        let e = res.clone().slice([0..n, 1..2]);
+        let stage1 = Tensor::<B, 2>::from_floats([[24.0]], &res.device());
+        let stage2 = Tensor::<B, 2>::from_floats([[24.0]], &res.device());
+        let term1 = o.mul(stages.clone());
+        let term2 = e.mul(stage1.sub(stages.clone()));
+        let numerator = term1.add(term2);
+        self.sigmoid.forward(numerator.div(stage2))
+    }
+    pub fn forward_regression(
+        &self,
+        features: Tensor<B, 2>,
+        stages: Tensor<B, 2, Float>,
+        targets: Tensor<B, 2, Float>,
+    ) -> RegressionOutput<B> {
+        let output = self.forward(features, stages);
+        let loss = MseLoss::new().forward(
+            output.clone(),
+            targets.clone(),
+            burn::nn::loss::Reduction::Auto,
+        );
+
+        RegressionOutput::new(loss, output, targets)
+    }
+}
+
+fn train<B: AutodiffBackend>(dataset: &str, artifact: &str, device: B::Device) {
+    let lines = match read_lines(dataset) {
+        Ok(lines) => {
+            println!("Dataset loaded: {} items", lines.len());
+            Ok(lines)
+        }
+        Err(e) => {
+            eprintln!("Error loading dataset: {}", e);
+            Err(e)
+        }
+    }
+    .unwrap();
+
+    let (lines_train, lines_valid) = split_lines(lines);
+    let train_dataset = MainDataset::new(lines_train);
+    let valid_dataset = MainDataset::new(lines_valid);
+
+    let batcher_train = BoardBatcher::<B>::new(device.clone());
+    let batcher_valid = BoardBatcher::<B::InnerBackend>::new(device.clone());
+
+    let config = TrainingConfig::new(ModelConfig { _unused: false }, AdamConfig::new());
+
+    let dataloader_train = DataLoaderBuilder::new(batcher_train)
+        .batch_size(config.batch_size)
+        .shuffle(config.seed)
+        .num_workers(config.num_workers)
+        .build(train_dataset);
+
+    let dataloader_valid = DataLoaderBuilder::new(batcher_valid)
+        .batch_size(valid_dataset.len())
+        .shuffle(config.seed)
+        .num_workers(config.num_workers)
+        .build(valid_dataset);
+
+    let learner = LearnerBuilder::new(artifact)
+        .metric_train_numeric(LossMetric::new())
+        .metric_valid_numeric(LossMetric::new())
+        .devices(vec![device.clone()])
+        .num_epochs(config.num_epochs)
+        .summary()
+        .build(
+            config.model.init::<B>(&device),
+            config.optimizer.init(),
+            config.learning_rate,
+        );
+
+    let _model_trained = learner.fit(dataloader_train, dataloader_valid);
+    let weights = get_layer_weights(&_model_trained.linear);
+
+    let o_pawn = median(&mut weights[8..=56][0].to_vec());
+    let e_pawn = median(&mut weights[8..=56][1].to_vec());
+
+    let weights: Vec<Vec<f32>> = weights
+        .iter()
+        .map(|row| {
+            let mut new_row = row.clone();
+            new_row[0] /= (o_pawn + e_pawn) / 2.0;
+            new_row[0] *= 100.0;
+            new_row[1] /= (o_pawn + e_pawn) / 2.0;
+            new_row[1] *= 100.0;
+            new_row
+        })
+        .collect();
+
+    println!("{:?}", weights);
+    println!("{:?} {:?}", o_pawn, e_pawn);
+}
+
+pub fn learn_model(dataset: &str, artifact: &str) {
+    type Backend = NdArray<f32>;
+    type AutodiffBackend = Autodiff<Backend>;
+    let device = NdArrayDevice::Cpu;
+    train::<AutodiffBackend>(dataset, artifact, device);
+}
+
+fn median(numbers: &mut [f32]) -> f32 {
+    numbers.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    numbers[numbers.len() / 2]
+}
+
+fn get_layer_weights<B: Backend>(linear_layer: &Linear<B>) -> Vec<Vec<f32>> {
+    let weight_data = linear_layer.weight.to_data();
+
+    let shape = weight_data.shape.clone();
+    let weights_slice = weight_data.as_slice::<f32>().unwrap();
+
+    let mut weights = Vec::with_capacity(shape[0]);
+    for row in 0..shape[0] {
+        let mut row_weights = Vec::with_capacity(shape[1]);
+        for col in 0..shape[1] {
+            row_weights.push(weights_slice[row * shape[1] + col]);
+        }
+        weights.push(row_weights);
+    }
+
+    weights
+}
