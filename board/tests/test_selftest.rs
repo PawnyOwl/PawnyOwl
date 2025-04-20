eddd5d99a803b43105c25cf753d010bdb64f522c
Alexander Kernozhitsky <sh200105@mail.ru>
2025-04-21 01:47:45 +0200

    Ignore test_selftest

--- board/tests/test_selftest.rs
+++ board/tests/test_selftest.rs
@@ -8,6 +8,7 @@ const INPUT_DATA: &str = include_str!("boards.fen");
 const OUTPUT_HASH: [u8; 32] =
     hex!("1ac232af9c1ede66b0cf423c87838324b09d178a5721b2c4ded7d87540a96318");
 
+#[ignore]
 #[test]
 fn test_selftest() {
     let mut hasher = Sha256::default();
